6a1acbc602e2b434e50462b7a2b6f9215c87e2e6
Riyzing <libia2k24@gmail.com>
2026-09-01 20:37:37 +0530

    2256b solving summary

--- DominoTiles.py
+++ DominoTiles.py
@@ -33,3 +33,18 @@ for _ in range(testCase):
             ans += 1
 
     print(ans)
+
+
+# Rating: 1000
+
+# What was the main idea? : the main idea was to create a pattern where ith pos != [i-2]th pos and only 4 pattern needed to be created using 00,01,11,10.
+
+# What did I initially think? i was unable to understand the idea first but with help i could understand the basics of what i need to do
+
+# Where did I get stuck? i got stuck in programing the main logic where i need to generate the pattern of the input length of the string while also match it condintion of the input string.
+
+# What observation unlocked it? : youtube video of some iit bhu dude helped me unlock the idea , i couldnt 
+
+# What technique was used? only one loop where the patterns were generated and then checked with the input string.
+
+# Could I recognize this technique in another problem? : no this was the first time i faced this type of problem.   
