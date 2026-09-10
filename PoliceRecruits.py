d289dea387732b6c83b037c7d880e7284b24d744
Riyzing <libia2k24@gmail.com>
2026-09-10 23:44:02 +0530

    427a 800

---
+++ PoliceRecruits.py
@@ -0,0 +1,18 @@
+tc = int(input())
+events = list(map(int, input().split()))
+
+police = 0
+untreated = 0
+
+for event in events:
+
+    if event > 0:
+        police += event
+
+    else:
+        if police > 0:
+            police -= 1
+        else:
+            untreated += 1
+
+print(untreated)
